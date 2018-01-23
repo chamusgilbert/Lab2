4911603e5a5979c2d4c000c69e067a1e13376719
famouschamus <chamusg@gmail.com>
2018-01-23 14:57:19 -0500

    Personalized with name greeting

--- lab2/Program.cs
+++ lab2/Program.cs
@@ -7,14 +7,17 @@ namespace lab2
         static void Main(string[] args)
         {
             int integer, oddEven;
-            string more;
-            Console.WriteLine("Please give me a number between 1 and 100");
+            string more, name;
+
+            Console.WriteLine("Hello, what is your name?");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello " + name + ", Please give me a number between 1 and 100");
             integer = int.Parse(Console.ReadLine());
 
             while (integer > 99 || integer < 2)
             {
-                Console.WriteLine("I said between 1 and 100!");
-                Console.WriteLine("Please give me a number between 1 and 100");
+                Console.WriteLine("I said between 1 and 100! " + name + ", get it together!");
+                Console.WriteLine("Please give me a number between 1 and 100, " + name);
                 integer = int.Parse(Console.ReadLine());
                 if (integer > 1 || integer < 100)
                 {
@@ -26,40 +29,43 @@ namespace lab2
 
             if (oddEven == 1 && integer > 60)
             {
-                Console.WriteLine(integer + " Odd");
+                Console.WriteLine(integer + "... that's Odd and over 60, " + name);
             }
             else if (oddEven == 1)
             {
-                Console.WriteLine(integer + " Odd");
+                Console.WriteLine(integer + "... that's Odd");
             }
             else if (oddEven == 0 && integer < 26)
             {
-                Console.WriteLine("Even and less than 25");
+                Console.WriteLine("That is even and less than 25, " + name);
             }
             else if (oddEven == 0 && integer > 25 && integer < 61)
             {
-                Console.WriteLine("Even");
+                Console.WriteLine("That's even, " + name);
             }
             else if (oddEven == 0 && integer > 60)
             {
-                Console.WriteLine(integer + " Even");
+                Console.WriteLine(integer + "... that's Even and over 60, " + name);
             }
             else
             {
                 Console.WriteLine("I'm Confused");
             }
 
-            Console.WriteLine("Would you like to continue? y/n");
+            Console.WriteLine("Would you like to continue, " + name + "? y/n");
             more = Console.ReadLine().ToUpper();
+
             while (more == "Y" || more == "YES")
             {
+
+
                 Console.WriteLine("Please give me a number between 1 and 100");
                 integer = int.Parse(Console.ReadLine());
 
                 while (integer > 99 || integer < 2)
                 {
-                    Console.WriteLine("I said between 1 and 100!");
-                    Console.WriteLine("Please give me a number between 1 and 100");
+                    Console.WriteLine("I said between 1 and 100! " + name + ", get it together!");
+                    Console.WriteLine("Please give me a number between 1 and 100, " + name);
                     integer = int.Parse(Console.ReadLine());
                     if (integer > 1 || integer < 100)
                     {
@@ -71,23 +77,23 @@ namespace lab2
 
                 if (oddEven == 1 && integer > 60)
                 {
-                    Console.WriteLine(integer + " Odd");
+                    Console.WriteLine(integer + "... that's Odd and over 60, " + name);
                 }
                 else if (oddEven == 1)
                 {
-                    Console.WriteLine(integer + " Odd");
+                    Console.WriteLine(integer + "... that's Odd");
                 }
                 else if (oddEven == 0 && integer < 26)
                 {
-                    Console.WriteLine("Even and less than 25");
+                    Console.WriteLine("That is even and less than 25, " + name);
                 }
                 else if (oddEven == 0 && integer > 25 && integer < 61)
                 {
-                    Console.WriteLine("Even");
+                    Console.WriteLine("That's even, " + name);
                 }
                 else if (oddEven == 0 && integer > 60)
                 {
-                    Console.WriteLine(integer + " Even");
+                    Console.WriteLine(integer + "... that's Even and over 60, " + name);
                 }
                 else
                 {
@@ -102,6 +108,7 @@ namespace lab2
                 }
 
             }
+
             Console.WriteLine("Goodbye");
         }
     }
